63da46f23fdf100c867acc609f9e99c654faf2b9
Giovanni Donati <grdonati99@gmail.com>
2024-12-02 06:19:41 -0300

    connection with the database and route post

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,60 +1,64 @@
 from http import HTTPStatus
 
+from sqlalchemy import create_engine,select
+from sqlalchemy.orm import Session
 from fastapi import FastAPI, HTTPException
 
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from fast_zero.settings import Settings
 
 app = FastAPI()
 
-database = []
-
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message' : 'Welcome to my aplicattion!'}
 
 @app.get('/users/', response_model=UserList)
 def read_users():
-    return {'users': database}
-
+    pass
 
 @app.get('/users/{id}', response_model=UserPublic)
 def read_user(id: int):
-    if id < 1 or id > len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-
-    user_with_id = database[id - 1]
-
-    return user_with_id
-
+    pass
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    engine = create_engine(Settings().DATABASE_URL)
 
-    database.append(user_with_id)
-    return user_with_id
+    with Session(engine) as session:
+        db_user = session.scalar(
+            select(User).where(
+                (User.user == user.user) | (User.email == user.email)
+            )
+        )
 
+        if db_user:
+            if db_user.username == user.user:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='User already exists'
+                )
+
+            elif db_user.email == user.email:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Email already exists'
+                )
+        
+        db_user = User(
+            user=user.user, email=user.email, password=user.password
+            )
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    return db_user
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-
-    user_with_id = UserDB(id=user_id, **user.model_dump())
-
-    database[user_id - 1] = user_with_id
-
-    return user_with_id
-
+    pass
 
 @app.delete('/users/{user_id}', response_model=Message)
 def delete_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-
-    del database[user_id - 1]
-
-    return {'message': 'user deleted!'}
+    pass
