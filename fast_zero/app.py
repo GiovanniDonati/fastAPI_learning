4b816f80bae0e590e1b3441a878f4396add81fe6
Giovanni_DeskEmp <grdonati99@gmail.com>
2024-12-03 13:32:59 -0300

    authentication and validation token

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,19 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
@@ -31,7 +38,10 @@ def read_user(id: int, session: Session = Depends(get_session)):
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session=Depends(get_session)):
+def create_user(
+    user: UserSchema,
+    session: Session = Depends(get_session),
+):
     db_user = session.scalar(
         select(User).where(
             (User.user == user.user) | (User.email == user.email)
@@ -42,7 +52,7 @@ def create_user(user: UserSchema, session=Depends(get_session)):
         if db_user.user == user.user:
             raise HTTPException(
                 status_code=HTTPStatus.BAD_REQUEST,
-                detail='User already exists',
+                detail='Username already exists',
             )
         elif db_user.email == user.email:
             raise HTTPException(
@@ -50,7 +60,14 @@ def create_user(user: UserSchema, session=Depends(get_session)):
                 detail='Email already exists',
             )
 
-    db_user = User(user=user.user, email=user.email, password=user.password)
+    hashed_password = get_password_hash(user.password)
+
+    db_user = User(
+        email=user.email,
+        user=user.user,
+        password=hashed_password,
+    )
+
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -59,33 +76,63 @@ def create_user(user: UserSchema, session=Depends(get_session)):
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session=Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    current_user.user = user.user
+    current_user.email = user.email
+    current_user.password = get_password_hash(user.password)
 
-    if not db_user:
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.BAD_REQUEST, detail='Not enough permission'
         )
 
-    db_user.user = user.user
-    db_user.password = user.password
-    db_user.email = user.email
     session.commit()
-    session.refresh(db_user)
+    session.refresh(current_user)
 
-    return db_user
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session=Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-
-    if not db_user:
+def delete_user(
+    user_id: int,
+    session=Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.BAD_REQUEST, detail='Not enough permission'
         )
 
-    session.delete(db_user)
+    session.delete(current_user)
     session.commit()
 
     return {'message': 'User deleted!'}
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ def test_create_user(client):
         json={
             'user': 'Giovanni',
             'email': 'teste@teste.com.br',
-            'password': '123',
+            'password': 'teste',
         },
     )  # Act (ação)
 
@@ -38,27 +38,44 @@ def test_read_users_with_user(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            'id': 1,
+            'id': user.id,
             'user': 'Donati',
             'email': 'teste@teste.com.br',
             'password': '123',
         },
     )
 
-    # assert response.status_code == HTTPStatus.OK
+    print(response.json())
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'id': 1,
+        'id': user.id,
         'user': 'Donati',
         'email': 'teste@teste.com.br',
     }
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        )
 
     # assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted!'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
